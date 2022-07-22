088a23f8fb9fd185e9aea869f2c014b8c89172b0
Javier Rodriguez <xjavierx1995@gmail.com>
2022-07-21 22:36:06 -0400

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]

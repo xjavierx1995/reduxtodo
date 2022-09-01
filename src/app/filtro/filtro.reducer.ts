ea08e01d48f14bea22e9612ca2bc32918f33cba9
Javier Rodriguez <xjavierx1995@gmail.com>
2022-09-01 18:02:24 -0400

    hasta aqui el 51 viene el 52

---
+++ src/app/filtro/filtro.reducer.ts
@@ -0,0 +1,10 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { filtrosValidos, setFiltro } from './filtro.actions';
+
+export const estadoInicial: filtrosValidos = 'todos'
+
+export const _filtroReducer = createReducer<filtrosValidos, Action>(estadoInicial,
+
+    on(setFiltro, (state, { filtro }) => filtro ),
+    
+);
